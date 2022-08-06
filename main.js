364fd7ac82209042b65fccc11c19af5bd0b904a4
aiswarya <aiswaryavalsan@gmail.com>
2022-08-06 11:17:21 -0500

    studentage

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
-var obj={num:2}
-var addToThis=function(a,b,c){
-    return this.num+a+b+c;
+var student={age:20}
+var addToThis=function(){
+    return this.age;
 }
-var bound=addToThis.bind(obj);
-console.log(bound(1,2,5));
+var bound=addToThis.bind(student);
+console.log(bound());
