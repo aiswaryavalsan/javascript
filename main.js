320225f05418d82b5c2106ae8b45f44514cdbf3a
aiswarya <aiswaryavalsan@gmail.com>
2022-08-06 11:12:58 -0500

    apply

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 var obj={num:2}
-var addToThis=function(a){
-    return this.num+a;
+var addToThis=function(a,b,c){
+    return this.num+a+b+c;
 }
-console.log(addToThis.call(obj,5));
+var arr=[1,2,3];
+console.log(addToThis.apply(obj,arr));
