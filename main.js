3b655d461aa3c50f38878e2f17b069a922f9e93f
aiswarya <aiswaryavalsan@gmail.com>
2022-08-06 11:14:52 -0500

    bind

--- main.js
+++ main.js
@@ -2,5 +2,5 @@ var obj={num:2}
 var addToThis=function(a,b,c){
     return this.num+a+b+c;
 }
-var arr=[1,2,3];
-console.log(addToThis.apply(obj,arr));
+var bound=addToThis.bind(obj);
+console.log(bound(1,2,5));
