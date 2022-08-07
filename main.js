8dc666c9b2f21b128f1893f0dfc3ec0b3d5701e9
aiswarya <aiswaryavalsan@gmail.com>
2022-08-07 05:47:08 -0500

    fatarrows3

--- main.js
+++ main.js
@@ -1,10 +1,15 @@
 "use strict"
-var x=function(){
-    var that=this;
-    this.val=1;
-    setTimeout(function(){
-        that.val++;
-        console.log(that.val)},1)
+/*var x=function(){
+    //var that=this;
+    this.val=4;
+    setTimeout(()=>{
+        this.val++;
+        console.log(this.val)},1)
 
     };
-    var xx=new x();
+    var xx=new x();*/
+     
+    var abc=(...n)=>{
+        console.log(n);
+    };
+    abc(1,2,3);
