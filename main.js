629d2e7896ae60e6173687378a26db94f2c67930
aiswarya <aiswaryavalsan@gmail.com>
2022-08-07 05:28:07 -0500

    faarow2

--- main.js
+++ main.js
@@ -1,7 +1,10 @@
-let getA=a=>a*a;
-console.log(getA(4));
-var a=4;
-let square=()=>{return (a*a)};
-console.log(square(6));
-let multiply=(a,b)=>{return (a*b)};
-console.log(multiply(4,6));
+"use strict"
+var x=function(){
+    var that=this;
+    this.val=1;
+    setTimeout(function(){
+        that.val++;
+        console.log(that.val)},1)
+
+    };
+    var xx=new x();
